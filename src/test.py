a1c3338ba44cd8a1dc02ecb0585f2f774475e34b
jayryuki <jayryuki@gmail.com>
2020-11-24 21:21:13 -0800

    fix test function

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from os.path import isfile, join
 import os
 import pandas as pd
 
-def test_data(datadir, outdir, ):
+def test_data(datadir, outdir, evaluatedir):
     report = open(r'test_report.txt', 'w+')
     for file in os.listdir(datadir):
         current = os.path.join(datadir, file)
