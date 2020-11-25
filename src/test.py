573586adb03cb4efc15c28be2d26babdc433a2d4
jayryuki <jayryuki@gmail.com>
2020-11-24 23:25:36 -0800

    asd

--- src/test.py
+++ src/test.py
@@ -25,6 +25,7 @@ def test_data(datadir, outdir, ate, rpe):
     print(odom)
     print(gt)
     print("python" + " " + slam + " " + gt + " " + "--plot ate.pdf")
+    print('mkdir -p /' + outdir)
     os.system('mkdir -p /' + outdir)
   
     os.system("python" + " " + ate + " " + slam + " " + gt + " " +  "--plot ate.png")
