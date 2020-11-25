1752ddebe369bc33ab4c317c9020ca09b292dab3
jayryuki <jayryuki@gmail.com>
2020-11-24 22:59:06 -0800

    changes

--- src/test.py
+++ src/test.py
@@ -6,8 +6,23 @@ import sys
 
 def test_data(datadir, outdir, evaluatedir):
     report = open(r'test_report.txt', 'w+')
+    slam = ''
+    odom = ''
+    gt = ''
     for file in os.listdir(datadir):
         current = os.path.join(datadir, file)
+        if 'slam' in current:
+            slam += current
+        
+        if 'odom' in current:
+            odom += current
+        
+        if 'gt' in current:
+            gt += current
 
-        os.system("python " + evaluatedir + "test")
-        print(current)
+
+    print(slam)
+    print(odom)
+    print(gt)
+    #os.system("python" + " " + evaluatedir + " " + current + " " + )
+        
