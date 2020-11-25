48498901e68a5b321000998d5570fc6e4b0d0982
jayryuki <jayryuki@gmail.com>
2020-11-24 22:46:21 -0800

    put in test python file

--- src/test.py
+++ src/test.py
@@ -2,9 +2,12 @@ from os import listdir
 from os.path import isfile, join
 import os
 import pandas as pd
+import sys
 
 def test_data(datadir, outdir, evaluatedir):
     report = open(r'test_report.txt', 'w+')
     for file in os.listdir(datadir):
         current = os.path.join(datadir, file)
+
+        os.system("script2.py 1")
         print(current)
