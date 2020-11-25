8849c5e7b4c868c57109d751b87a927137802c9f
jayryuki <jayryuki@gmail.com>
2020-11-25 03:56:31 -0800

    test

--- src/test.py
+++ src/test.py
@@ -42,7 +42,7 @@ def test_data(datadir, test_resultsdir, metricsdir, plotsdir, ate, rpe):
         rpe = os.popen('python' + ' ' + rpe + ' ' + odom + ' ' + gt + ' ' +  '--plot rpe_' + str(counter) + '.png --fixed_delta').read()
         
         to_write = ate + ',' + rpe + ',' + metric
-        to_write = to_write.replace('\r\n', '')
+        to_write = to_write.replace('\n', '')
         print('\n' in to_write)
         report.write(to_write)
         os.system('mv ate_' + str(counter) + '.png ' + plotsdir)
